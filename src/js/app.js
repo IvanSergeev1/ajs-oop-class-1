a4bc4ede2c54c0b192d5d9925c41aca4019c06da
IvanSergeev1 <57568287+IvanSergeev1@users.noreply.github.com>
2020-07-31 17:03:30 +0300

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+export default class Character {
+  constructor(name, type) {
+    const typeArr = [
+      'Bowman',
+      'Swordsman',
+      'Magician',
+      'Daemon',
+      'Undead',
+      'Zombie',
+    ];
+
+    if (name.length < 2 || name.length > 10 || typeof name !== 'string') {
+      throw new Error('Ошибка! Неправильное имя персонажа');
+    }
+
+    if (!typeArr.includes(type)) {
+      throw new Error('Ошибка! Задан несуществующий тип персонажа');
+    }
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+  }
+}
